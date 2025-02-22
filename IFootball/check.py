7f7e9e516d97015c552eb256249ced81fafaac68
Nafi1548 <masuqalislamnafi@gmail.com>
2025-02-22 14:00:13 +0600

    get_comp_stats sql functionified

--- IFootball/check.py
+++ IFootball/check.py
@@ -1,4 +1,6 @@
 from Queries import Queries
 
-x = Queries.get_competition_standings(2002)
-print(x)
+x = Queries.get_competition_stats(2018)
+y = Queries.get_competition_statstest(2018)
+
+print(len(x),len(y),x==y)
